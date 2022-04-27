148c3c3c438c1f85a16cb43018721c419c380da9
hamzazirh <hamza_zirh@hotmail.com>
2022-04-27 22:08:24 +0300

    Video modal js ayarlamaları yapıldı.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".home-slider").slick({
         autoplay: true,
         dots: false,
@@ -26,6 +26,17 @@ $(document).ready(function(){
             if ($("header.bg")[0]) { $("header.bg").removeClass("bg") }
         }
     });
+
+    var videoModal = new bootstrap.Modal(document.getElementById('videoModal'), {
+        keyboard: false
+    })   
+
+
+    $('[data-videoid]').click(function () {
+        videoModal.toggle()
+        var videoId = $(this).data('videoid')
+        $('#videoModal .modal-body').html(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
+    })
 
 
 
